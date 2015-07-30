b8212a929108242a7d31de645eb8f5d23e068411
harrysa66 <harrysa66@163.com>
2015-07-30 18:00:58 +0800

    重写角色equals

--- loveAdmin/src/main/java/com/love/system/po/Role.java
+++ loveAdmin/src/main/java/com/love/system/po/Role.java
@@ -2,6 +2,7 @@ package com.love.system.po;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import org.apache.ibatis.type.Alias;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,69 @@ public class Role implements Serializable {
 	private Date modifyTime;
 	private String status;
 	private String isvalid;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result
+				+ ((createTime == null) ? 0 : createTime.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((isvalid == null) ? 0 : isvalid.hashCode());
+		result = prime * result
+				+ ((modifyTime == null) ? 0 : modifyTime.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (createTime == null) {
+			if (other.createTime != null)
+				return false;
+		} else if (!createTime.equals(other.createTime))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (isvalid == null) {
+			if (other.isvalid != null)
+				return false;
+		} else if (!isvalid.equals(other.isvalid))
+			return false;
+		if (modifyTime == null) {
+			if (other.modifyTime != null)
+				return false;
+		} else if (!modifyTime.equals(other.modifyTime))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
 
 	public String getId() {
 		return this.id;
